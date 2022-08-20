1cf1b675e29dfa917e8f3a3fbd182a166d5a7dda
diana nthenya odhiambo <dianaodhiambo1096@gmail.com>
2022-08-20 22:26:25 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(drivers,name){
+    return drivers = drivers.filter(driver => driver === name || driver == name.toLowerCase())
+
+}
+
+function fuzzyMatch(drivers, letters){
+    return drivers.filter(driver => driver.indexOf(letters) == 0)
+}
+
+function matchName(drivers, name){
+    return drivers.filter(drivers => drivers.name === name);
+}
